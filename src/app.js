da87457d0e95dca623689759e0cd4bf3cf66d335
Nataraja-g-v <nataraja.gv@ticketsque.com>
2025-12-10 00:11:47 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const ConnectDB = require("./config/data-base");
+const cors = require("cors");
 
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routers/auth-router");
@@ -10,6 +11,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
